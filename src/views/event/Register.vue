ed6d43da18785f1a73a3d4c69731c9f4f4314620
José David Rodriguez Payo <josedavidrp12@gmail.com>
2025-05-25 21:30:45 +0200

    Navegación por código en página de registro

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,7 +1,16 @@
 <script setup>
+import { useRouter } from 'vue-router'
 const props = defineProps(['event'])
+const router = useRouter()
+const register = () => {
+  //Si el registro es correcto, volver a los detalles del evento
+  router.push({
+    name: 'EventDetails',
+  })
+}
 </script>
 
 <template>
   <p>Apúntate al evento aquí</p>
+  <button @click="register">¡Apúntame!</button>
 </template>
