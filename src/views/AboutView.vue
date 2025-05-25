b33220b73075a0519adcef738ab1417a0988b850
José David Rodriguez Payo <josedavidrp12@gmail.com>
2025-05-25 19:01:13 +0200

    Cambios en Vue Router y nombres de las vistas

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A site for events to better the world.</h1>
+    <h3>
+      <a href="https://linkedin.com/in/jdavid-rodriguez" target="_blank"
+        >José David Rodríguez Payo</a
+      >
+    </h3>
   </div>
 </template>
